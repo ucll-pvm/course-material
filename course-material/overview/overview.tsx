cb2794d579440e495a98ebd6143f921578ae555c
Frederic Vogels <frederic.vogels@ucll.be>
2022-02-26 17:04:07 +0100

    course-material/overview

--- course-material/overview/overview.tsx
+++ course-material/overview/overview.tsx
@@ -3,20 +3,21 @@ import styled from 'styled-components';
 
 
 
-const TopicBlock = styled.div`
+const TopicHtml = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-    width: 40%;
-    padding: 0.2em;
+    padding-left: 1em;
+    padding-right: 1em;
     font-variant: small-caps;
     margin: 1px auto;
     &:nth-child(odd) { background: #BBF; }
     &:nth-child(even) { background: #BBF; }
+    width: 100%;
 `;
 
-const TopicChildren = styled.div`
+const TopicChildrenHtml = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: flex-end;
@@ -24,122 +25,175 @@ const TopicChildren = styled.div`
     gap: 0.2em;
 `;
 
-const TopicChildBlock = styled.div`
+const TopicChildHtml = styled.div`
     text-decoration-line: none;
     & a {
         text-decoration-line: none;
     }
 `;
 
-const WeekBlock = styled.div`
-    width: 2em;
-    height: 2em;
-    border-style: inset;
-    text-align: center;
-    line-height: 2em;
-`;
+const SlideBlock = styled(TopicChildHtml)``;
 
-const SlideBlock = styled(TopicChildBlock)``;
+const ExerciseBlock = styled(TopicChildHtml)``;
 
-const ExerciseBlock = styled(TopicChildBlock)``;
-
-const TopicDescription = styled.div`
+const TopicDescriptionHtml = styled.div`
     display: flex;
     flex-direction: row;
     justify-contents: flex-start;
     align-items: center;
-    gap: 1em;
 `;
 
-const TopicName = styled.div`
+const TopicNameHtml = styled.div`
     height: 2em;
     line-height: 2em;
+    font-size: 125%;
 `;
 
-function Topic(props : { name: string, week: number, children ?: React.ReactNode })
+const WeekContainerHtml = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-contents: flex-start;
+    width: 100%;
+    margin: 0.2em;
+`;
+
+const WeekLabelHtml = styled.div`
+    writing-mode: tb;
+    text-orientation: mixed;
+    transform: rotate(180deg);
+    text-align: center;
+    font-weight: bold;
+    color: white;
+    background: black;
+    font-size: 125%;
+    font-variant: small-caps;
+    padding: 0.5em;
+`;
+
+const WeekChildrenContainerHtml = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-contents: flex-start;
+    width: 100%;
+`;
+
+const OverviewHtml = styled.div`
+    margin: 1em auto;
+    width: 50%;
+`;
+
+function Topic(props : { name: string, children ?: React.ReactNode })
 {
     return (
-        <TopicBlock>
-            <TopicDescription>
-                <Week week={props.week}/>
-                <TopicName>
+        <TopicHtml>
+            <TopicDescriptionHtml>
+                <TopicNameHtml>
                     {props.name}
-                </TopicName>
-            </TopicDescription>
-            <TopicChildren>
+                </TopicNameHtml>
+            </TopicDescriptionHtml>
+            <TopicChildrenHtml>
                 {props.children}
-            </TopicChildren>
-        </TopicBlock>
+            </TopicChildrenHtml>
+        </TopicHtml>
     );
 }
 
 function Slides(props : { id: string })
 {
+    const url = `/slides/topics/${props.id}`;
+
     return (
         <SlideBlock>
-            <a href={props.id}>📖</a>
+            <a href={url}>📖</a>
         </SlideBlock>
     );
 }
 
 function Exercises(props : { id: string })
 {
+    const url = `labs/${props.id}/assignment.html`;
+
     return (
         <ExerciseBlock>
-            <a href={props.id}>🔨</a>
+            <a href={url}>🔨</a>
         </ExerciseBlock>
     );
 }
 
-function Week(props: {week : number})
+function WeekContainer(props : { week : number, children ?: React.ReactNode })
 {
     return (
-        <WeekBlock>
-            {props.week}
-        </WeekBlock>
+        <WeekContainerHtml>
+            <WeekLabelHtml>
+                week {props.week}
+            </WeekLabelHtml>
+            <WeekChildrenContainerHtml>
+                {props.children}
+            </WeekChildrenContainerHtml>
+        </WeekContainerHtml>
     );
 }
 
 export function Overview(props : {})
 {
     return (
-        <>
-            {topic(1, 'C++ Introduction', true, false, 'cpp-intro')}
-            {topic(1, 'Declarations', true, true)}
-            {topic(1, 'Preprocessor', true, true)}
-            {topic(2, 'Allocation Methods', true, false)}
-            {topic(2, 'Pointers', true, true)}
-            {topic(3, "Heap Allocation", true, true)}
-            {topic(3, "Arrays", true, true)}
-            {topic(3, "Containers", true, true)}
-            {topic(4, "Const", true, false)}
-            {topic(4, "Exercise on types", false, true, 'types')}
-            {topic(4, "References", true, true)}
-            {topic(5, "Classes (overview)", true, false)}
-            {topic(5, "Constructors", true, false)}
-            {topic(5, "Structs", true, false)}
-            {topic(5, "sizeof", true, false)}
-            {topic(5, "Const Correctness", true, false)}
-            {topic(5, "Classes", false, true)}
-            {topic(6, "Default Parameter Values", true, true)}
-            {topic(6, "Operator Overloading", true, true)}
-            {topic(6, "RAII", true, false)}
-            {topic(6, "Smart Pointers", true, true)}
-            {topic(7, "Inheritance", true, true)}
-            {topic(7, "Templates", true, true)}
-            {topic(7, "Casting", true, true)}
-            {topic(8, "Performance", true, true)}
-            {topic(8, "Technical Details", true, false)}
-        </>
+        <OverviewHtml>
+            <WeekContainer week={1}>
+                {topic('C++ Introduction', true, false, 'cpp-intro')}
+                {topic('Declarations', true, true)}
+                {topic('Preprocessor', true, true)}
+            </WeekContainer>
+            <WeekContainer week={2}>
+                {topic('Allocation Methods', true, false)}
+                {topic('Pointers', true, true)}
+            </WeekContainer>
+            <WeekContainer week={3}>
+                {topic("Heap Allocation", true, true)}
+                {topic("Arrays", true, true)}
+                {topic("Containers", true, true)}
+            </WeekContainer>
+            <WeekContainer week={4}>
+                {topic("Const", true, false)}
+                {topic("Exercise on types", false, true, 'types')}
+                {topic("References", true, true)}
+            </WeekContainer>
+            <WeekContainer week={5}>
+                {topic("Classes (overview)", true, false)}
+                {topic("Constructors", true, false)}
+                {topic("Structs", true, false)}
+                {topic("sizeof", true, false)}
+                {topic("Const Correctness", true, false)}
+                {topic("Classes", false, true)}
+            </WeekContainer>
+            <WeekContainer week={6}>
+                {topic("Default Parameter Values", true, true)}
+                {topic("Operator Overloading", true, true)}
+                {topic("RAII", true, false)}
+                {topic("Smart Pointers", true, true)}
+            </WeekContainer>
+            <WeekContainer week={7}>
+                {topic("Inheritance", true, true)}
+                {topic("Templates", true, true)}
+                {topic("Casting", true, true)}
+            </WeekContainer>
+            <WeekContainer week={8}>
+                {topic("Bit Manipulation", true, false)}
+                {topic("Compression: Introduction", true, false, 'compression-intro')}
+                {topic("Compression: Theory", true, false, 'compression-theory')}
+                {topic("Compression: RLE", true, false, 'compression-rle')}
+                {topic("Compression: Huffman", true, false, 'compression-huffman')}
+                {topic("Compression: LZ77", true, false, 'compression-lz77')}
+            </WeekContainer>
+        </OverviewHtml>
     );
 
 
-    function topic(week : number, name : string, slides : boolean, exercises : boolean, id ?: string)
+    function topic(name : string, slides : boolean, exercises : boolean, id ?: string)
     {
         id = id || name.toLocaleLowerCase().replaceAll(' ', '-');
 
         return (
-            <Topic name={name} week={week}>
+            <Topic name={name}>
                 {slides ? <Slides id={id} /> : <></>}
                 {exercises ? <Exercises id={id} /> : <></>}
             </Topic>
