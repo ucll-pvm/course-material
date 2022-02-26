bc149f68543bc7b8c0820906c9c7cca3b87c4752
Frederic Vogels <frederic.vogels@ucll.be>
2022-02-26 13:54:26 +0100

    course-material/overview

--- course-material/overview/overview.tsx
+++ course-material/overview/overview.tsx
@@ -113,12 +113,14 @@ export function Overview(props : {})
             {topic(3, "Arrays", true, true)}
             {topic(3, "Containers", true, true)}
             {topic(4, "Const", true, false)}
+            {topic(4, "Exercise on types", false, true, 'types')}
             {topic(4, "References", true, true)}
             {topic(5, "Classes (overview)", true, false)}
             {topic(5, "Constructors", true, false)}
             {topic(5, "Structs", true, false)}
             {topic(5, "sizeof", true, false)}
             {topic(5, "Const Correctness", true, false)}
+            {topic(5, "Classes", false, true)}
             {topic(6, "Default Parameter Values", true, true)}
             {topic(6, "Operator Overloading", true, true)}
             {topic(6, "RAII", true, false)}
@@ -144,158 +146,3 @@ export function Overview(props : {})
         );
     }
 }
-
-
-// export class Overview extends React.Component<IProps, IState>
-// {
-//     render()
-//     {
-//         // const me = this;
-
-//         return (
-//             <Table className={this.props.className}>
-//                 <tbody>
-//                     <HeaderRow>
-//                         <WeekHeader>Week</WeekHeader>
-//                         <Header>Topic</Header>
-//                         <Header>Type</Header>
-//                     </HeaderRow>
-//                     {this.range(this.firstWeek, this.lastWeek + 1).map(i => this.renderWeek(i))}
-//                 </tbody>
-//             </Table>
-//         );
-//     }
-
-//     private get firstWeek() : number { return 1; }
-
-//     private get lastWeek() : number { return 8; }
-
-//     private renderWeek(n : number) : JSX.Element
-//     {
-//         const rows = this.weekRows(n);
-
-//         const elts = this.range(0, rows.length).map(i => {
-//             if ( i === 0 )
-//             {
-//                 return (
-//                     <Row>
-//                         <Week rowSpan={rows.length}>{n}</Week>
-//                         {rows[i]}
-//                     </Row>
-//                 );
-//             }
-//             else
-//             {
-//                 return (
-//                     <Row>
-//                         {rows[i]}
-//                     </Row>
-//                 );
-//             }
-//         });
-
-//         return <React.Fragment>{elts}</React.Fragment>;
-//     }
-
-//     private weekRows(n : number) : JSX.Element[]
-//     {
-//         const weeks : JSX.Element[][] = [];
-
-//         weeks[1] = [
-//             theory('C++ Introduction', 'cpp-intro'),
-//             theory('Declarations'),
-//             lab('Declarations'),
-//             theory('Preprocessor'),
-//             lab('Preprocessor'),
-//         ];
-
-//         weeks[2] = [
-//             theory('Allocation Methods'),
-//             theory('Pointers'),
-//             lab('Pointers'),
-//         ];
-
-//         weeks[3] = [
-//             theory('Heap Allocation'),
-//             lab('Heap Allocation'),
-//             theory('Arrays'),
-//             lab('Arrays'),
-//             theory('Containers'),
-//         ];
-
-//         weeks[4] = [
-//             theory('Const'),
-//             lab('Types'),
-//             theory('References'),
-//             lab('References'),
-//         ];
-
-//         weeks[5] = [
-//             theory('Classes (overview)', 'classes-overview'),
-//             theory('Constructors'),
-//             theory('Structs'),
-//             theory('sizeof'),
-//             theory('Const Correctness'),
-//             lab('Types 2'),
-//             lab('Classes'),
-//         ];
-
-//         weeks[6] = [
-//             theory('Default Parameter Values'),
-//             lab('Default Parameter Values'),
-//             theory('Operator Overloading'),
-//             lab('Operator Overloading'),
-//             theory('RAII'),
-//             theory('Smart Pointers'),
-//             lab('Smart Pointers'),
-//         ];
-
-//         weeks[7] = [
-//             theory('Inheritance'),
-//             lab('Inheritance'),
-//             theory('Templates'),
-//             lab('Templates'),
-//             theory('Casting', 'casts'),
-//             lab('Casting', 'casts'),
-//         ];
-
-//         weeks[8] = [
-//             theory('Performance (optional)', 'performance'),
-//             lab('Performance (optional)', 'performance'),
-//             theory('Bit Manipulation'),
-//             theory('Technical Details'),
-//         ];
-
-//         return weeks[n];
-
-
-//         function theory(caption : string, filename : string | null = null) : JSX.Element
-//         {
-//             if ( filename === null )
-//             {
-//                 filename = caption.toLowerCase().replace(' ', '-');
-//             }
-
-//             const url = `topics/${filename}.pdf`;
-
-//             return <TheoryEntry caption={caption} url={url} />
-//         }
-
-//         function lab(caption : string, directoryName : string | null = null) : JSX.Element
-//         {
-//             if ( directoryName === null )
-//             {
-//                 directoryName = caption.toLowerCase();
-//             }
-
-//             const url = `labs/${directoryName}/index.html`;
-
-//             return <LabEntry caption={caption} url={url} />
-//         }
-//     }
-
-//     private range(a : number, b : number) : number[]
-//     {
-//         return a < b ? [ a, ...this.range(a + 1, b) ] : [];
-//     }
-// }
